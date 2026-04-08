3f2d22f09b5472daf2d50921996dc7f9be8f3fb1
Chiranth M Benakal BCA <chiranthmbenakalbca25@rvu.edu.in>
2026-04-08 23:37:11 +0530

    Add files via upload

---
+++ OS_lab3.sh
@@ -0,0 +1,13 @@
+echo "job 1:Display Date"
+date
+echo "job 2:Display current user"
+whoami
+echo "job 3:Display Calender"
+cal
+echo "job 4:List of files"
+ls
+echo "job 5:Display my system data"
+date +"%T"
+echo "job6"
+pwd
+echo "All jobs are finished  "
